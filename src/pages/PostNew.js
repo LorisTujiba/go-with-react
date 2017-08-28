60297684e022bb27ccd414d087cfdd58215ddef8
Loris Tujiba <pisceslrz@gmail.com>
2017-08-28 13:17:59 +0700

    fixed form

--- src/pages/PostNew.js
+++ src/pages/PostNew.js
@@ -1,46 +1,94 @@
 import React, {Component} from 'react'
 import Header from '../components/Header';
-import { reduxForm } from 'redux-form';
-import { Grid, FormGroup, FormControl, ControlLabel }from 'react-bootstrap';
+import _ from 'lodash';
+import { Field, reduxForm } from 'redux-form';
+import { Grid, Panel, FormGroup, FormControl, ControlLabel, Button, ButtonToolbar, HelpBlock }from 'react-bootstrap';
 
 class PostNew extends Component{
 
+	renderField(field){
+		console.log('field ',field)
+	  	const className = field.meta.error && field.meta.touched
+                  ? "error"
+                  : !field.meta.error
+                    ? "success"
+                    : null;
+
+	    return(
+	        <FormGroup validationState={className}>
+	          <ControlLabel>
+	            {field.label}
+	          </ControlLabel>
+	          <FormControl
+	            type="text"
+	            {...field.input}
+	          />
+	          <FormControl.Feedback/>
+	          <HelpBlock>
+	            {field.meta.touched ? field.meta.error : ''}
+	          </HelpBlock>
+	        </FormGroup>
+	    )
+	}	
+
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render(){
+		const { handleSubmit } = this.props;
+
 		return(
 			<div>
 				<Header />
 				<Grid>
-					<form>
-						<FormGroup
-							controlId="formBasicText"
-						>
-							<ControlLabel>
-								Title
-							</ControlLabel>
-							<FormControl
-								type="text"
-								placeholder="Enter title here ..."
-							>
-
-							</FormControl>
-						</FormGroup><FormGroup
-							controlId="formBasicText"
-						>
-							<ControlLabel>
-								Body
-							</ControlLabel>
-							<FormControl
-								type="text"
-								placeholder="Enter body here ..."
-							>
-
-							</FormControl>							
-						</FormGroup>
-					</form>
+					<Panel header="Insert New Post" bsStyle="success">
+						<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+							<Field
+				              label="Title"
+				              name="title"
+				              component={this.renderField}
+				            />
+				            <Field
+				              label="Tag"
+				              name="tag"
+				              component={this.renderField}
+				            />
+				            <Field
+				              label="Body"
+				              name="body"
+				              component={this.renderField}
+				            />			
+				            <ButtonToolbar>
+								<Button bsStyle="primary">Submit</Button>
+								<Button bsStyle="danger">Cancel</Button>
+							</ButtonToolbar>
+						</form>
+					</Panel>
 				</Grid>
 			</div>
 		)		
 	}
 }
 
-export default PostNew;
+function validate(values){
+	const errors = {};
+	console.log(values)
+
+	if(!values.title || values.title.length < 3){
+    	errors.title = "Enter a title that is at least 3 characters!";
+  	}
+	if(!values.tag){
+	 	errors.tag = "Enter some tag";
+	}
+	if(!values.body){
+	  	errors.body = "Enter some body";
+  	}
+
+  return errors;
+}
+
+export default reduxForm({
+	validate,
+	form : 'PostsNew'
+})(PostNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducers from './users';
+import { reducer  as formReducer } from 'redux-form';
 import postReducers from './posts';
 
 const rootReducers = combineReducers({
   users : userReducers,
-  posts : postReducers
+  posts : postReducers,
+  form : formReducer
 })
 
 export default rootReducers;
